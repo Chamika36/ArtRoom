d645dc1e183db2374d8e169502cae6b7274647b7
Chamika Madhushan <chamikamadhushan35@gmail.com>
2023-09-11 23:42:11 +0530

    initial views added

---
+++ app/views/home/login.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="C:\xampp\htdocs\ArtRoom\public\css\style.css" />
+
+    <style>
+      a,
+      button,
+      input,
+      select,
+      h1,
+      h2,
+      h3,
+      h4,
+      h5,
+      * {
+        margin: 0;
+        padding: 0;
+        border: none;
+        text-decoration: none;
+        appearance: none;
+        background: none;
+      }
+    </style>
+    <title>Document</title>
+  </head>
+  <body>
+    <div class="login">
+      <img class="rectangle-147" src="rectangle-147.png" />
+      <svg
+        class="ellipse-1"
+        width="60"
+        height="63"
+        viewBox="0 0 60 63"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M60 31.5C60 48.897 46.5685 63 30 63C13.4315 63 0 48.897 0 31.5C0 14.103 13.4315 0 30 0C46.5685 0 60 14.103 60 31.5Z"
+          fill="#1E1E1E"
+        />
+      </svg>
+
+      <div class="login2">Login<br /></div>
+      <svg
+        class="rectangle-5"
+        width="103"
+        height="39"
+        viewBox="0 0 103 39"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M0 19.5C0 8.73045 8.73045 0 19.5 0H83.5C94.2696 0 103 8.73045 103 19.5C103 30.2696 94.2696 39 83.5 39H19.5C8.73045 39 0 30.2696 0 19.5Z"
+          fill="#1E1E1E"
+        />
+      </svg>
+
+      <div class="group-1">
+        <svg
+          class="rectangle-3"
+          width="232"
+          height="37"
+          viewBox="0 0 232 37"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            opacity="0.6"
+            d="M0 18.5C0 8.28273 8.28273 0 18.5 0H213.5C223.717 0 232 8.28273 232 18.5C232 28.7173 223.717 37 213.5 37H18.5C8.28273 37 0 28.7173 0 18.5Z"
+            fill="#D9D9D9"
+          />
+        </svg>
+
+        <svg
+          class="rectangle-4"
+          width="232"
+          height="37"
+          viewBox="0 0 232 37"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            opacity="0.6"
+            d="M0 18.5C0 8.28273 8.28273 0 18.5 0H213.5C223.717 0 232 8.28273 232 18.5C232 28.7173 223.717 37 213.5 37H18.5C8.28273 37 0 28.7173 0 18.5Z"
+            fill="#D9D9D9"
+          />
+        </svg>
+
+        <div class="password">Password</div>
+        <div class="username">Username</div>
+      </div>
+      <div class="forgot-password">Forgot Password ?</div>
+      <div class="don-t-have-an-account">Don’t have an account?</div>
+      <div class="log-in">LOG IN</div>
+      <svg
+        class="rectangle-1"
+        width="1054"
+        height="36"
+        viewBox="0 0 1054 36"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M0 0H1054V36H0V0Z" fill="#37312B" />
+      </svg>
+
+      <div class="sign-up">SIGN UP</div>
+      <svg
+        class="flip-backward"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M3 9H16.5C18.9853 9 21 11.0147 21 13.5C21 15.9853 18.9853 18 16.5 18H12M3 9L7 5M3 9L7 13"
+          stroke="black"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </div>
+  </body>
+</html>
