d20b0868417e91976c778debdb7e3b984592f42f
Thevindu Rathnayake <2021is079@stu.ucsc.cmb.ac.lk>
2023-11-02 23:38:40 +0530

    modified partner home page

--- app/views/pages/partner/home.php
+++ app/views/pages/partner/home.php
@@ -5,31 +5,140 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/home.css">
     <title>Home</title>
+
+    <style>
+ .availability-container {
+  display: flex;
+  align-items: center;
+  justify-content: center; /* Center horizontally */
+  margin: 20px auto; /* Center horizontally, set vertical margin as needed */
+  padding: 20px;
+  background-color: #f0f0f0;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  width: 600px;
+  
+}
+
+
+.availability-container label {
+  margin-right: 10px;
+}
+
+.availability-container input[type="checkbox"] {
+  width: 20px; /* Reduce the width of the checkbox */
+  height: 20px; /* Set the height of the checkbox */
+  margin: 0; /* Remove default margin */
+  transform: scale(1); /* Reset the scale for the checkbox */
+}
+
+/* Style the checkbox when it's checked */
+.availability-container input[type="checkbox"]:checked + label::before {
+  content: '\2714'; /* Unicode checkmark symbol */
+  color: #00b300; /* Green color */
+  font-size: 20px;
+}
+
+/* Style the checkbox when it's not checked */
+.availability-container input[type="checkbox"] + label::before {
+  content: '\2717'; /* Unicode cross symbol */
+  color: #ff0000; /* Red color */
+  font-size: 20px;
+}
+
+.availability-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 20px auto;
+  padding: 20px;
+  background-color: #f0f0f0;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  width: 600px;
+}
+
+.availability-container label {
+  margin-right: 10px;
+}
+
+.availability-container input[type="checkbox"] {
+  width: 20px;
+  height: 20px;
+  margin: 0;
+  transform: scale(1);
+}
+
+.availability-container input[type="checkbox"]:checked + label::before {
+  content: '\2714';
+  color: #00b300;
+  font-size: 20px;
+}
+
+.availability-container input[type="checkbox"] + label::before {
+  content: '\2717';
+  color: #ff0000;
+  font-size: 20px;
+}
+
+.shape-container {
+  display: flex;
+  justify-content: center; /* Center horizontally */
+  margin-top: 4%; /* Adjust the margin as needed to move it down */
+}
+
+
+
+.shape {
+  width: 220px;
+  height: 180px;
+  background-color: #BFA586;
+  border: 1px solid #333;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 20px;
+  font-weight: bold;
+  font-size: 25px;
+  border-radius: 15px;
+  transition: transform 0.3s; /* Add a smooth transition for the transform property */
+}
+
+.shape:hover {
+  transform: scale(1.1); /* Scale the shape when the mouse is over it */
+}
+
+
+
+
+    </style>
+
 </head>
 <body>
     <?php include(APPROOT . '/views/include/partner-navbar.php'); ?>
 
-    <div class="container bg-orange text-center text-brown p-3 mt-5 col-4" style="border-radius: 50px;">
-        <div class="row">
-            <div class="col">
-                <label class="d-flex align-items-center justify-content-center" style="font-weight: bold; font-size: xx-large;">
-                    <span class="mr-4" style="margin-right: 20px;">My Availability</span>
-                    <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadio1" value="option1">
-                        <label class="form-check-label" for="exampleRadio1">On</label>
-                    </div>
-                    <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadio2" value="option2">
-                        <label class="form-check-label" for="exampleRadio2">Off</label>
-                    </div>
-                </label>
-            </div>
-        </div>
-    </div>
-    <div class="position-absolute top-0 end-0 d-flex justify-content-center align-items-center" style="margin-top: 310px; margin-right: 60px; width: 50px; height: 50px; background-color: #ff8c00; border-radius: 50%;">
-        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 16 16">
-            <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z"/>
-        </svg>
-    </div>
+    <div class="availability-container">
+  <label for="available-checkbox">Available</label>
+  <input type="checkbox" id="available-checkbox" name="availability">
+</div>
+
+<div class="availability-container">
+  <label for="not-available-checkbox">Not Available</label>
+  <input type="checkbox" id="not-available-checkbox" name="availability">
+</div>
+
+<div class="shape-container">
+  <div class="shape">
+    <p>Number of Requests</p>
+  </div>
+  <div class="shape">
+    <p>Ongoing</br> Events</p>
+  </div>
+  <div class="shape">
+    <p>Completed</br> Events</p>
+  </div>
+</div>
+
 </body>
 </html>
