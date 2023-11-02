d4e65a10c65641787e0a3aeee5d7edce5a72e6fe
Chamika Madhushan <chamikamadhushan35@gmail.com>
2023-11-02 09:26:58 +0530

    Manager access control

---
+++ app/views/errors/404.view.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Something Went Wrong</title>
+</head>
+<body>
+  <div class="error-container">
+    <h1><span class="oops">Oops!</span>  Something Went Wrong</h1>
+    <p>We're sorry, but something went wrong. Please try again later.</p>
+    <a href="<?php echo URLROOT ?>/home">Go back to the homepage</a>
+  </div>
+</body>
+</html>
