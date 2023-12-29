10af4a238f8821675399b7ac047a4c134d38c92c
Thevindu Rathnayake <2021is079@stu.ucsc.cmb.ac.lk>
2023-12-29 18:48:43 +0530

    modified the home nav bar when the customer is not logged in

--- app/views/include/customer-navbar.php
+++ app/views/include/customer-navbar.php
@@ -9,40 +9,32 @@
 
 <body>
     <!-- Navbar -->
-    <div class="navbar">
-        <nav>          
-            <ul>
-                <li><a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                    <path d="M12 19V5M12 5L5 12M12 5L19 12" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
-                    </a>
-                </li>
-                <!-- Common Items for All Users -->
-                <?php
-                    if (isset($_SESSION['user_id'])) {
-                        // User is logged in
-                        echo '<li class="right"><a href="'. URLROOT .'/users/logout">Log Out</a></li>';
-                    } else {
-                        // User is not logged in
-                        echo '<li class="right"><a href="'. URLROOT .'/users/login">Log In</a></li>';
-                    }
-                ?>
-                <li><a href="<?php echo URLROOT ?>/home">Home</a></li>
-                <li><a href="<?php echo URLROOT ?>/events/request">Request</a></li>
-                <li><a href="<?php echo URLROOT ?>/packages">Packages</a></li>
-                <li><a href="<?php echo URLROOT ?>/samples"">Gallery</a></li>
-                <li><a href="#">Reviews</a></li>
+        <header>
+            <div class="branding">
+                <img src="<?php echo URLROOT ?>/images/logo.png" alt="Your Image">
+                <span>Art Room</span>
+            </div>
+            
+            <nav>
+                <ul>
+                    <!-- Common Items for All Users -->
+                    <?php
+                        if (isset($_SESSION['user_id'])) {
+                            // User is logged in
+                            echo '<li><a href="'. URLROOT .'/users/logout">Log Out</a></li>';
+                        } else {
+                            // User is not logged in
+                            echo '<li>You are not Logged In</li>';
+                            echo '<li><a href="'. URLROOT .'/users/login">Log In</a></li>';
+                        }
+                    ?>
+                    <!-- User-Specific Items (Added Dynamically using PHP) -->
+                </ul>
+            </nav>
+        </header>
                 
-                <li><a href="#">Contact Us</a></li>
-
-                
-                
-                <!-- User-Specific Items (Added Dynamically using PHP) -->
-            </ul>
-        </nav>
-
        
-    </div>
+    
     <!-- Content of the Page Goes Here -->
 </body>
 </html>
