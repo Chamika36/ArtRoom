419da11f2d7d2ad0142a73062ca2c758789dc5e3
Thevindu Rathnayake <2021is079@stu.ucsc.cmb.ac.lk>
2023-12-31 12:07:39 +0530

    changes made in nav bar (after logged in)

--- app/views/include/customer-navbar.php
+++ app/views/include/customer-navbar.php
@@ -9,32 +9,60 @@
 
 <body>
     <!-- Navbar -->
-        <header>
-            <div class="branding">
-                <img src="<?php echo URLROOT ?>/images/logo.png" alt="Your Image">
-                <span>Art Room</span>
-            </div>
-            
-            <nav>
-                <ul>
-                    <!-- Common Items for All Users -->
-                    <?php
-                        if (isset($_SESSION['user_id'])) {
-                            // User is logged in
-                            echo '<li><a href="'. URLROOT .'/users/logout">Log Out</a></li>';
-                        } else {
-                            // User is not logged in
-                            echo '<li>You are not Logged In</li>';
-                            echo '<li><a href="'. URLROOT .'/users/login">Log In</a></li>';
-                        }
-                    ?>
-                    <!-- User-Specific Items (Added Dynamically using PHP) -->
-                </ul>
-            </nav>
-        </header>
-                
-       
-    
+    <header>
+        <div class="branding">
+            <img src="<?php echo URLROOT ?>/images/logo.png" alt="Your Image">
+            <span>Art Room</span>
+        </div>
+        
+        <nav>
+            <ul>
+                <!-- Common Items for All Users -->
+                <?php
+                    if (isset($_SESSION['user_id'])) {
+                        // User is logged in
+                        
+                        echo '<li class="profile-dropdown">';
+                        echo '<div class="profile-info" id="profileTrigger">';
+                        echo '<div class="user-greeting">Hello User</div>'; // Move greeting here
+                        echo '<img src="' . URLROOT . '/images/pp.png" alt="Profile Picture">';
+                        echo '<div class="arrow-icon">&#9660;</div>';
+                        echo '</div>';
+                        echo '<div class="dropdown-content" id="dropdownMenu">';
+                        echo '<a href="'. URLROOT .'/users/logout">Log Out</a>';
+                        echo '<a href="'. URLROOT .'/requests">Requests</a>';
+                        echo '</div>';
+                        echo '</li>';
+                    } else {
+                        // User is not logged in
+                        echo '<li>You are not Logged In</li>';
+                        echo '<li><a href="'. URLROOT .'/users/login">Log In</a></li>';
+                    }
+                ?>
+            </ul>
+        </nav>
+    </header>
+
     <!-- Content of the Page Goes Here -->
+
+    <script>
+        document.getElementById('profileTrigger').addEventListener('click', function() {
+            var dropdown = document.getElementById('dropdownMenu');
+            dropdown.classList.toggle('show');
+        });
+
+        // Close the dropdown if the user clicks outside of it
+        window.onclick = function(event) {
+            if (!event.target.matches('#profileTrigger, #profileTrigger *')) {
+                var dropdown = document.getElementById('dropdownMenu');
+                if (dropdown.classList.contains('show')) {
+                    dropdown.classList.remove('show');
+                }
+            }
+        };
+    </script>
 </body>
+
+
+
 </html>
