5eb4663822938346d2dc8d2a0efbcbb4a1aaa475
proxypeat <2021is024@stu.ucsc.cmb.ac.lk>
2024-04-14 07:43:21 +0530

    ad image

--- app/controllers/Samples.php
+++ app/controllers/Samples.php
@@ -102,54 +102,59 @@ class Samples extends Controller{
             // Sanitize POST data
             $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
             
-            // Init data
-            $data = array(
-                'name' => trim($_POST['name']), 
-                'imagePath' => $_POST['imagePath'],
-                'description' => trim($_POST['description']),
-                'customer' => trim($_POST['customer']),
-                'date' => trim($_POST['date']),
-                'name_err' => '',
-                'imagePath_err' => '',
-                'description_err' => '',
-                'customer_err' => '',
-                'date_err' => ''
-            );
+            // File upload
+            $image_name = $_FILES['image']['name'];
+            $image_tmp_name = $_FILES['image']['tmp_name'];
+            $image_err = $_FILES['image']['error'];
     
-            // Validate Name
-            if(empty($data['name'])) {
-                $data['name_err'] = 'Please enter name';
-            }
+            // Check for file upload error
+            if($image_err === 0){
+                // Generate a unique name for the image
+                $image_new_name = uniqid('', true) . '_' . $image_name;
+                // Set the path where the image will be stored
+                $image_destination = 'images/samples/' . $image_new_name;
     
-            // Validate Image Path
-            if(empty($data['imagePath'])) {
-                $data['imagePath_err'] = 'Please enter image path';
-            }
+                // Move the uploaded file to the destination folder
+                if(move_uploaded_file($image_tmp_name, $image_destination)){
+                    // Init data
+                    $data = array(
+                        'name' => trim($_POST['name']), 
+                        'imagePath' => $image_new_name,
+                        'description' => trim($_POST['description']),
+                        'customer' => trim($_POST['customer']),
+                        'date' => trim($_POST['date']),
+                        'name_err' => '',
+                        'imagePath_err' => '',
+                        'description_err' => '',
+                        'customer_err' => '',
+                        'date_err' => ''
+                    );
     
-            // Validate Description
-            if(empty($data['description'])) {
-                $data['description_err'] = 'Please enter description';
-            }
+                    // Validate Name, Description, Date, and Customer here...
     
-            // Validate Date
-            if(empty($data['date'])) {
-                $data['date_err'] = 'Please enter date';
-            }
-    
-            // Make sure no errors
-            if(empty($data['name_err']) && empty($data['imagePath_err']) && empty($data['description_err']) && empty($data['customer_err']) && empty($data['date_err'])) {
-                // Validated
-                if($this->sampleModel->addSample($data)) {
-                    flash('sample_message', 'Sample Added');
-                    redirect('samples');
+                    // Make sure no errors
+                    if(empty($data['name_err']) && empty($data['description_err']) && empty($data['customer_err']) && empty($data['date_err'])) {
+                        // Validated
+                        if($this->sampleModel->addSample($data)) {
+                            flash('sample_message', 'Sample Added');
+                            redirect('samples');
+                        } else {
+                            die('Something went wrong');
+                        }
+                    } else {
+                        // Load view with errors
+                        $this->view('pages/manager/samples/addsample', $data);
+                    }
                 } else {
-                    die('Something went wrong');
+                    // Error uploading file
+                    $data['image_err'] = 'Error uploading file';
+                    $this->view('pages/manager/samples/addsample', $data);
                 }
             } else {
-                // Load view with errors
+                // No file uploaded
+                $data['image_err'] = 'Please upload an image';
                 $this->view('pages/manager/samples/addsample', $data);
             }
-    
         } else {
             $customers = $this->userModel->getCustomers();
     
--- app/models/Sample.php
+++ app/models/Sample.php
@@ -1,104 +1,68 @@
 <?php
+    class Sample{
+        private $db;
 
-require_once '../app/init.php';
+        public function __construct() {
+            $this->db = new Database;
+        }
 
-class Sample {
-    private $db;
+        public function getSamples() {
+            $this->db->query('SELECT * FROM Sample');
+            $results = $this->db->resultSet();
+            return $results;
+        }
 
-    public function __construct() {
-        $this->db = new Database;
-    }
-
-    public function getSamples() {
-        $this->db->query('SELECT * FROM Sample');
-        $results = $this->db->resultSet();
-        return $results;
-    }
-
-    public function getSampleById($id) {
-        $this->db->query('SELECT * FROM Sample WHERE SampleID = :id');
-        $this->db->bind(':id', $id);
-        $row = $this->db->single();
-        if ($row) {
+        public function getSampleById($id) {
+            $this->db->query('SELECT * FROM Sample WHERE SampleID = :id');
+            $this->db->bind(':id', $id);
+            $row = $this->db->single();
             return $row;
-        } else {
-            return null; // Or any default value you prefer
         }
-    }
 
-    public function addSample($data) {
-        $this->db->beginTransaction();
-        try {
+        public function addSample($data) {
             $this->db->query('INSERT INTO Sample (SampleName, ImagePath, Description, CustomerID, Date) VALUES (:name, :imagePath, :description, :customer, :date)');
-    
-            // Check if $data['imagePath'] is an array
-            if (is_array($data['imagePath'])) {
-                // Loop through each uploaded file
-                foreach ($data['imagePath'] as $image) {
-                    // Generate unique filename
-                    $fileName = uniqid() . '_' . $image['name'];
-                    // Move uploaded file to desired folder
-                    move_uploaded_file($image['tmp_name'], '../public/images/sample/' . $fileName);
-                    // Bind values
-                    $this->db->bind(':name', $data['name']);
-                    $this->db->bind(':imagePath', $fileName);
-                    $this->db->bind(':description', $data['description']);
-                    $this->db->bind(':customer', $data['customer']);
-                    $this->db->bind(':date', $data['date']);
-                    // Execute query for each image
-                    $this->db->execute();
-                }
+            // Bind values
+            $this->db->bind(':name', $data['name']);
+            $this->db->bind(':imagePath', $data['imagePath']);
+            $this->db->bind(':description', $data['description']);
+            $this->db->bind(':customer', $data['customer']);
+            $this->db->bind(':date', $data['date']);
+
+            if($this->db->execute()) {
+                return true;
             } else {
-                // Handle the case where only one file is uploaded
-                $fileName = uniqid() . '_' . $data['imagePath']['name'];
-                move_uploaded_file($data['imagePath']['tmp_name'], '../public/images/sample/' . $fileName);
-                // Bind values
-                $this->db->bind(':name', $data['name']);
-                $this->db->bind(':imagePath', $fileName);
-                $this->db->bind(':description', $data['description']);
-                $this->db->bind(':customer', $data['customer']);
-                $this->db->bind(':date', $data['date']);
-                // Execute query for single image
-                $this->db->execute();
+                return false;
             }
-    
-            $this->db->commit();
-            return true;
-        } catch (\PDOException $e) { // Corrected to \PDOException
-            // Rollback if any exception occurs
-            $this->db->rollBack();
-            return false;
         }
-    }
 
-    // Delete Sample
-    public function deleteSample($id) {
-        $this->db->query('DELETE FROM Sample WHERE SampleID = :id');
-        // Bind values
-        $this->db->bind(':id', $id);
+        // Delete Sample
+        public function deleteSample($id){
+            $this->db->query('DELETE FROM Sample WHERE SampleID = :id');
+            // Bind values
+            $this->db->bind(':id', $id);
 
-        if ($this->db->execute()) {
-            return true;
-        } else {
-            return false;
+            if($this->db->execute()) {
+                return true;
+            } else {
+                return false;
+            }
         }
-    }
 
-    // Update Sample
-    public function updateSample($data) {
-        $this->db->query('UPDATE Sample SET SampleName = :name, ImagePath = :imagePath, Description = :description, Date = :date WHERE SampleID = :id');
-        // Bind values
-        $this->db->bind(':id', $data['id']);
-        $this->db->bind(':name', $data['name']);
-        $this->db->bind(':imagePath', $data['imagePath']);
-        $this->db->bind(':description', $data['description']);
-        $this->db->bind(':date', $data['date']);
+        // Update Sample
+        public function updateSample($data){
+            $this->db->query('UPDATE Sample SET SampleName = :name, ImagePath = :imagePath, Description = :description, Date = :date WHERE SampleID = :id');
+            // Bind values
+            $this->db->bind(':id', $data['id']);
+            $this->db->bind(':name', $data['name']);
+            $this->db->bind(':imagePath', $data['imagePath']);
+            $this->db->bind(':description', $data['description']);
+            $this->db->bind(':date', $data['date']);
 
-        if ($this->db->execute()) {
-            return true;
-        } else {
-            return false;
+            if($this->db->execute()) {
+                return true;
+            } else {
+                return false;
+            }
         }
+
     }
-}
-?>
