8547ca965ea5efc9f5802290de9decbc642e5a5b
Chamika Madhushan <chamikamadhushan35@gmail.com>
2024-04-20 14:37:47 +0530

    sample: updated buttons and views

--- app/views/pages/manager/samples/viewsample.php
+++ app/views/pages/manager/samples/viewsample.php
@@ -10,20 +10,31 @@
 
 <?php include(APPROOT . '/views/include/sidebar/manager-sidebar.php'); ?>
 
-<div class="container">
-    <div class="sample-info">
-        <h2><?php echo $data['sample']->SampleName; ?></h2>
-       
-        <p><?php echo $data['sample']->Description; ?></p>
-        <p>Date: <?php echo $data['sample']->Date; ?></p>
-    </div>
-   
+
+<div class="home">
+
+<section id = "portfolio" class = "vh-100 py-7">
+    <div class="container">
+        <div class = "portfolio-content">
+            <div class = "sample-info">
+                <h2><?php echo $data['sample']->SampleName; ?></h2>
+            
+                <p><?php echo $data['sample']->Description; ?></p>
+                <p>Date: <?php echo $data['sample']->Date; ?></p>
+            </div>
+
     
-    <div class="sample-images">
-        <?php foreach ($data['images'] as $image) : ?>
-            <img src="<?php echo URLROOT ?>/<?php echo $image ?>" alt="Sample Image">
-        <?php endforeach; ?>
+        
+        <div class="portfolio-grid">
+            <?php foreach ($data['images'] as $image) : ?>
+                <div>
+                    <img src="<?php echo URLROOT ?>/<?php echo $image ?>" alt="Sample Image">
+                </div>
+            <?php endforeach; ?>
+        </div>
+        </div>
     </div>
+
 </div>
 
 </body>
